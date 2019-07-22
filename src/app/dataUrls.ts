9ec437d3c619111e8ec9c94e687144a14192e1fb
nadav <1991nadav@gmail.com>
2019-07-22 14:00:50 +0300

    paths const to get data from server

---
+++ src/app/dataUrls.ts
@@ -0,0 +1,6 @@
+export const dataUrl = {
+  students: 'https://my-json-server.typicode.com/YonatanKra/demoStudentsServer/students',
+  courses: 'https://my-json-server.typicode.com/YonatanKra/demoStudentsServer/courses',
+  marks: 'https://my-json-server.typicode.com/YonatanKra/demoStudentsServer/marks',
+  allData: '- https://my-json-server.typicode.com/YonatanKra/demoStudentsServer/db'
+}
