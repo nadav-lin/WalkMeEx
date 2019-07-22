6f2941d230ab3993c6b1731d32ea30287df0cd7c
nadav <1991nadav@gmail.com>
2019-07-22 18:43:45 +0300

    created student component and refactored the table to use it

--- src/app/student-table/student-table.component.ts
+++ src/app/student-table/student-table.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {StudentService} from "../services/student.service";
 import {Student} from "../entities/student";
-import {MarksService} from "../services/marks.service";
-import {Mark} from "../entities/mark";
-import {CoursesService} from "../services/courses.service";
 
 @Component({
   selector: 'app-student-table',
@@ -12,7 +9,9 @@ import {CoursesService} from "../services/courses.service";
 })
 export class StudentTableComponent implements OnInit {
 
-  constructor(private studentService: StudentService, private marksService: MarksService, private coursesService: CoursesService) {
+  edit = false;
+
+  constructor(private studentService: StudentService) {
   }
 
   ngOnInit() {
@@ -20,13 +19,5 @@ export class StudentTableComponent implements OnInit {
 
   getStudents(): Student[]{
     return this.studentService.getStudents();
-  }
-
-  getAverageMarks(id: string): number{
-    return this.marksService.getAverageMarksofStudent(id);
-  }
-
-  getNumberOfCourses(id: string): number{
-    return this.coursesService.getCoursesOfStudent(id).length;
   }
 }
