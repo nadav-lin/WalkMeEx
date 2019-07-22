ad4375ba425c226d00c9f394d67ce6c02d14f41a
nadav <1991nadav@gmail.com>
2019-07-22 15:45:20 +0300

    basic app layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StudentTableComponent} from "./student-table/student-table.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'studentTable', component: StudentTableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
